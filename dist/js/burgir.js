7f8f6d06a9129ac7868a1c2732e6d0c62e8e18a4
StephenKhytze <stephen092madula@gmail.com>
2024-08-09 18:57:52 +0800

    tab transition

--- dist/js/burgir.js
+++ dist/js/burgir.js
@@ -8,19 +8,28 @@ burgirBtn.addEventListener('click', () => {
 
 const menuNav = document.querySelectorAll('.tab_nav_link');
 const menuContent = document.querySelectorAll('.menu_content_item');
+const menuTabs = document.querySelector('.menu_content')
 const tabIndicator = document.querySelector('.menu_tabs_indicator');
 
 menuNav.forEach((menu, index) => {
     menu.addEventListener('click', () => {
         removeActiveMenu();
         menu.classList.add('active');
-        
-        const activeContent = document.querySelectorAll(`#${menu.id}-content`);
-        removeActiveContent();
-        activeContent.forEach((content) => {
+        menuTabs.style.opacity = 0;
+        menuTabs.style.transform = "translateX(-30rem)"
+        setTimeout(() => {
+            menuTabs.style.transform = "translateX(30rem)"
+        }, 100);
+
+        setTimeout(() => {
+            const activeContent = document.querySelectorAll(`#${menu.id}-content`);
+            removeActiveContent();
+            activeContent.forEach((content) => {
             content.classList.add('active');
         });
-
+            menuTabs.style.transform = "translateX(0)"
+            menuTabs.style.opacity = 1;
+        }, 150);
         if (innerWidth <= 460) {
             tabIndicator.style.left = `calc(calc(33% * ${index}) + 4.35rem)`;
         } else if (innerWidth <= 1920) {
