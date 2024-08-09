cfc087345a2eb0cd9c3a0ac3c0755eb2125d0081
StephenKhytze <stephen092madula@gmail.com>
2024-08-09 18:37:00 +0800

    tab indicator

--- dist/js/burgir.js
+++ dist/js/burgir.js
@@ -7,18 +7,28 @@ burgirBtn.addEventListener('click', () => {
 });
 
 const menuNav = document.querySelectorAll('.tab_nav_link');
-const menuContent = document.querySelectorAll('.menu_content_item')
+const menuContent = document.querySelectorAll('.menu_content_item');
+const tabIndicator = document.querySelector('.menu_tabs_indicator');
 
-menuNav.forEach((menu) => {
+menuNav.forEach((menu, index) => {
     menu.addEventListener('click', () => {
         removeActiveMenu();
         menu.classList.add('active');
         
         const activeContent = document.querySelectorAll(`#${menu.id}-content`);
         removeActiveContent();
-        activeContent.forEach((content) => {content.classList.add('active')});
+        activeContent.forEach((content) => {
+            content.classList.add('active');
+        });
+
+        if (innerWidth <= 460) {
+            tabIndicator.style.left = `calc(calc(33% * ${index}) + 4.35rem)`;
+        } else if (innerWidth <= 1920) {
+            tabIndicator.style.left = `calc(calc(11.33% * ${index}) + 42.75rem)`;
+        }
     });
 });
+
 
 function removeActiveMenu() {
     menuNav.forEach((menu) => {
